2bac2f4b1302b4ed74bff67a4cd574d6382e7a02
Davi Lucas Gomes Feitosa <davi-06@hotmail.com>
2022-10-18 10:18:29 -0300

    XCCloud connect

--- XCCTest/ViewController.swift
+++ XCCTest/ViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    func olaMundo() {
-        print("hello word")
-    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hellou, world")
     }
 
 }
